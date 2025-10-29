a21f565d0a3614b1531eebf76e4e0388a629ec30
Danushari98 <danushari98@gmail.com>
2025-10-29 16:11:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     const [todos, setTodos] = useState([]);
 
     useEffect(() => {
-        axios.get('http://localhost:8000/api/todos/')
+        axios.get('https://todobackend-qosa.onrender.com/api/todos/')
             .then(res => setTodos(res.data));
     }, []);
 
